cb5dbbb21849f8d15f3aae19a6d0850f5f05351a
Matthew Wilkes <tonicblue@gmail.com>
2019-07-05 21:57:31 +0100

    Improved `Doer` test coverage

--- src/doers/doer.spec.ts
+++ src/doers/doer.spec.ts
@@ -1,5 +1,6 @@
 import { TaskConfig, Environment } from '../environment';
 import { BoundaryContext } from '../boundaries/boundary';
+import { CacheConfig } from '../cache-managers/cache-manager';
 
 test('should be able to execute doer and have input echoed', async () => {
   const env = new Environment({}, [], {
@@ -30,4 +31,54 @@ test('should be able to execute doer and have input echoed', async () => {
   await doer.execute(context, task);
 
   expect(context.data[task.name]).toStrictEqual(task.config);
+});
+
+test('should be able to execute doer and have input cached and returned', async () => {
+  const env = new Environment({}, [], {
+    metadata: {
+      test: 'It worked'
+    }
+  });
+  await env.init();
+
+  const doer = env.getDoer('Doer');
+  const cacheManager = env.getCacheManager('CacheManager');
+
+  const cacheConfig: CacheConfig = {
+    cacheManager: 'CacheManager',
+    keyProperties: ['env.config.metadata'],
+    partition: 'test',
+    ttl: 86400000
+  };
+
+  const task: TaskConfig = {
+    config: {
+      test: 'It worked'
+    },
+    cacheConfig: cacheConfig,
+    doer: 'Doer',
+    metadata: {},
+    name: 'test-doer'
+  };
+
+  const context: BoundaryContext = {
+    env: env,
+    boundary: { config: {}, input: {} },
+    data: {},
+    errors: {}
+  };
+
+  const cacheKey = await cacheManager.makeKey(cacheConfig, context);
+
+  await doer.execute(context, task);
+  const cacheCreated = cacheManager.CACHE[cacheKey.partition][cacheKey.key].touched;
+
+  expect(context.data[task.name]).toStrictEqual(task.config);
+
+  await new Promise((resolve) => { setTimeout(resolve, 2000) });
+
+  await doer.execute(context, task);
+  const cacheTouched = cacheManager.CACHE[cacheKey.partition][cacheKey.key].touched;
+
+  expect(+cacheCreated).toBeLessThan(+cacheTouched);
 });
