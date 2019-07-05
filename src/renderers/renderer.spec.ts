f71b0e9940f4e5947efb16b396722c57b6db0f17
Matthew Wilkes <tonicblue@gmail.com>
2019-07-05 21:39:03 +0100

    Finished `Renderer` tests

---
+++ src/renderers/renderer.spec.ts
@@ -0,0 +1,39 @@
+import { Environment, Context } from '../environment';
+import { RenderConfig } from './renderer';
+
+test('should echo input with no parser', async () => {
+  const env = new Environment({}, [], {});
+  await env.init();
+  const renderer = env.getRenderer('Renderer');
+  const config: RenderConfig = {
+    __renderer: 'Renderer',
+    __template: {
+      test: 'It worked'
+    }
+  };
+  const context: Context = {
+    env: env
+  };
+
+  const rendered = await renderer.render(config, context);
+
+  expect(rendered).toEqual(config.__template);
+});
+
+test('should echo parsed input', async () => {
+  const env = new Environment({}, [], {});
+  await env.init();
+  const renderer = env.getRenderer('Renderer');
+  const config: RenderConfig = {
+    __renderer: 'Renderer',
+    __template: 42,
+    __parser: 'StringParser'
+  };
+  const context: Context = {
+    env: env
+  };
+
+  const rendered = await renderer.render(config, context);
+
+  expect(rendered).toBe('42');
+});
