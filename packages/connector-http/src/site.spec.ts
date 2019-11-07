f20f5387a4e44f3bcccd154d451dda47663e5cd7
Matthew Wilkes <tonicblue@gmail.com>
2019-11-07 09:50:56 +0000

    More test coverage for HTTP connector

--- packages/connector-http/src/site.spec.ts
+++ packages/connector-http/src/site.spec.ts
@@ -3,24 +3,52 @@ import * as Path from 'path';
 
 // tslint:disable-next-line: no-implicit-dependencies
 import { transpile } from '@low-systems/json-i';
+import { Environment } from 'low';
 
-import { Site, SiteConfig } from './site';
+import { ConnectorHttp } from './connector-http';
 
-//TODO: Load an environment here instead, need tasks and registered routes
-//      to do our testing below...
-//      Also, finish these tests
-let siteConfig: SiteConfig | undefined;
-beforeAll(() => {
+let environment: Environment | undefined;
+beforeAll(async (done) => {
   const testDir = Path.join(__dirname, '..', 'test-data');
   const testPath = Path.join(testDir, 'main.json5');
   const testJson = FS.readFileSync(testPath).toString();
   const testData = transpile(testJson, testDir);
-  siteConfig = testData.modules.ConnectorHttp;
+
+  process.env.SECRETS = JSON.stringify(testData.secrets);
+
+  environment = new Environment({
+    connectors: [new ConnectorHttp()]
+  }, testData.tasks, testData.environment);
+
+  await environment.init();
+
+  done();
+});
+
+afterAll(async (done) => {
+  if (environment) {
+    await environment.destroy();
+    environment = undefined;
+  }
+
+  done();
 });
 
 test('should be able to match routes', () => {
-  if (!siteConfig) { fail('Site config has not been setup properly'); return; }
+  if (!environment) { fail('Environment has not been setup properly'); return; }
+  const connector = environment.getConnector('ConnectorHttp') as ConnectorHttp;
+  const site = connector.sites.default;
 
-  const site = new Site('test', siteConfig);
-  expect(site.matchRoute('/', 'GET'));
+  expect(site.matchRoute('/', 'GET').route.task.name).toBe('index');
+  expect(site.matchRoute('/index', 'GET').route.task.name).toBe('index');
+  expect(site.matchRoute('/index/', 'GET').route.task.name).toBe('index');
+
+  expect(site.matchRoute('/post/test-slug', 'GET').route.task.name).toBe('post');
+  expect(site.matchRoute('/post/test-slug', 'GET').params.slug).toBe('test-slug');
+  expect(() => { site.matchRoute('/post/test-slug', 'POST') }).toThrow(/invalid path/i);
+  expect(site.matchRoute('/post/fixed', 'GET').route.task.name).toBe('post-fixed');
+
+  site.config.stripTrailingSlash = false;
+
+  expect(() => { site.matchRoute('/index/', 'GET') }).toThrow(/invalid path/i);
 });
