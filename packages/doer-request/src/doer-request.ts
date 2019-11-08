28c3edc58e6dfb686efdcdc186174cd81b02895c
Matthew Wilkes <tonicblue@gmail.com>
2019-11-08 10:28:08 +0000

    Created Http Request doer with 100% test coverage

---
+++ packages/doer-request/src/doer-request.ts
@@ -0,0 +1,9 @@
+import * as Request from 'request-promise-native';
+
+import { Doer, TaskConfig, ConnectorContext } from 'low';
+
+export class DoerRequest extends Doer<any, any> {
+  async main(context: ConnectorContext<any>, taskConfig: TaskConfig, coreConfig: Request.Options): Promise<any> {
+    return await Request(coreConfig);
+  }
+}
