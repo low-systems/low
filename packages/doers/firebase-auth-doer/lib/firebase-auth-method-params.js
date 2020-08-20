66c4b2bbbf18aa1f8af13fe9807e4d98ba42a918
Matthew Parry-Wilkes <tonicblue@gmail.com>
2020-08-20 14:39:50 +0100

    Removing redundant files from firebase auth doer

--- packages/doers/firebase-auth-doer/lib/firebase-auth-method-params.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=firebase-auth-method-params.js.map
--- packages/doers/firebase-auth-doer/lib/firebase-get-user-doer.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=firebase-get-user-doer.js.map
