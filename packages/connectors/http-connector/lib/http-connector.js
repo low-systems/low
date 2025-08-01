57de16c6a4355942ae17870eb7325e9008bf5989
Matthew Parry-Wilkes <tonicblue@gmail.com>
2025-08-01 14:49:08 +0100

    made data a const to be passed in handle response

--- packages/connectors/http-connector/lib/http-connector.js
+++ packages/connectors/http-connector/lib/http-connector.js
@@ -186,15 +186,14 @@ class HttpConnector extends low_1.Connector {
                 input.headers = request.headers;
                 input.client = this.getClientInfo(input.headers, connection);
                 input.body = yield this.getRequestBody(request, input.site.config.getBodyOptions);
-                let data;
+                const data = {};
                 if (Array.isArray((_b = (_a = input.site) === null || _a === void 0 ? void 0 : _a.config) === null || _b === void 0 ? void 0 : _b.inputHandlers)) {
                     for (const handler of input.site.config.inputHandlers) {
                         const task = this.env.getTask(handler);
-                        const context = yield this.runTask(task, input, match.route.config);
+                        const context = yield this.runTask(task, input, match.route.config, data);
                         if (context.data.http_response) {
                             return this.sendResponse(response, context.data.http_response, input.site);
                         }
-                        data = context.data;
                     }
                 }
                 const context = yield this.runTask(match.route.task, input, match.route.config, data);
--- packages/connectors/http-connector/src/http-connector.ts
+++ packages/connectors/http-connector/src/http-connector.ts
@@ -175,18 +175,16 @@ export class HttpConnector extends Connector<HttpConnectorConfig, any, HttpInput
       input.client = this.getClientInfo(input.headers, connection);
       input.body = await this.getRequestBody(request, input.site.config.getBodyOptions);
 
-      let data: any;
+      const data: any = {};
 
       if (Array.isArray(input.site?.config?.inputHandlers)) {
         for (const handler of input.site.config.inputHandlers) {
           const task = this.env.getTask(handler);
-          const context = await this.runTask(task, input, match.route.config);
+          const context = await this.runTask(task, input, match.route.config, data);
 
           if (context.data.http_response) {
             return this.sendResponse(response, context.data.http_response, input.site);
           }
-
-          data = context.data;
         }
       }
 
